c751de814b5a0117fad93b9d4472a70e8691a25e
lauraahurtado <laura.hurtado@correo.unimet.edu.ve>
2024-08-08 04:26:29 -0400

    Creacion de las clases personaje y planeta del csv

---
+++ Planeta_cvs.py
@@ -0,0 +1,14 @@
+class Planeta_csv:
+    def __init__(self,id,nombre,diametro,periodo_de_rotacion,periodo_de_orbita,gravedad,poblacion,clima,terreno,superficie_acuatica,residentes,peliculas):
+        self.id=id
+        self.nombre=nombre
+        self.diametro=diametro
+        self.periodo_de_rotacion=periodo_de_rotacion
+        self.periodo_de_orbita=periodo_de_orbita
+        self.gravedad=gravedad
+        self.poblacion=poblacion
+        self.clima=clima
+        self.terreno=terreno
+        self.superficie_acuatica=superficie_acuatica
+        self.residentes=residentes
+        self.peliculas=peliculas
