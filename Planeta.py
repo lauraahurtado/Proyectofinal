90d48e431dcb4632074cbb4a65d071b4673eace9
alejandrobriceno2 <alejandro.briceno@correo.unimet.edu.ve>
2024-08-08 02:49:59 -0400

    Creacion clases: Especie, Nave, Pelicula, Personaje, Planeta

---
+++ Planeta.py
@@ -0,0 +1,13 @@
+class Planeta:
+    def __init__(self, nombre, diametro, periodo_de_rotacion, periodo_de_orbita, gravedad, poblacion, clima, terreno, superficie_acuatica):
+        self.nombre=nombre
+        self.diametro=diametro
+        self.periodo_de_rotacion=periodo_de_rotacion
+        self.periodo_de_orbita=periodo_de_orbita
+        self.gravedad=gravedad
+        self.poblacion=poblacion
+        self.clima=clima
+        self.terreno=terreno
+        self.superficie_acuatica=superficie_acuatica
+
+    
