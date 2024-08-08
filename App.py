d9530d507fc6acd17b1158d96e6405195d45c198
alejandrobriceno2 <alejandro.briceno@correo.unimet.edu.ve>
2024-08-08 03:41:08 -0400

    Creacion clase (Mision), se añadió el menu

--- App.py
+++ App.py
@@ -53,3 +53,62 @@ class App:
         except:
             print('''Está fallando la carga de la API, por conexión a internet u otro motivo.
                   Vuelva a correr el programa.''')
+            
+
+
+
+
+### MENÚ DEL PROGRAMA
+        while True:
+            menu=input('''Ingrese el índice numérico correspondiente a la opcion del menu que desea realizar:
+1. Ver lista de Peliculas
+2. Ver listado de todas las especies de la saga, ordenados por el ID
+3. Ver lista de planetas
+4. Buscar un personaje de la saga
+5. Gráfico de cantidad de personajes nacidos en cada planeta
+6. Gráficos de características de naves
+7. Tabla estadística sobre naves
+8. Construir Mision
+9. Modificar Mision                       
+10. Visualizar Mision                                           
+11. Salir
+--->:''')
+            
+            if menu=="1":
+                None
+
+
+            elif menu=='2':
+                None
+
+            elif menu=='3':
+                None
+
+            elif menu=='4':
+                None
+
+            elif menu=='5':
+                None
+
+            elif menu=='6':
+                None
+
+            elif menu=='7':
+                None
+
+            elif menu=='8':
+                None
+
+            elif menu=='9':
+                None
+
+            elif menu=='10':
+                None
+
+            elif menu=='11':
+                
+                print('Hasta la proxima aventura estelar')
+                break
+
+            else:
+                print('Ingrese una opcion contemplada en el menu: ')
