5d34d76475b86dfc118dae67d3ad00e1c7980539
alejandrobriceno2 <alejandro.briceno@correo.unimet.edu.ve>
2024-08-08 03:20:39 -0400

    Creacion App y adicion de un metodo de la Clase Planeta

---
+++ App.py
@@ -0,0 +1,55 @@
+### IMPORTACION DE EXTENSIONES Y CLASES
+
+import requests as rq
+import json
+from Pelicula import Pelicula
+from Especie import Especie
+from Nave import Nave
+from Personaje import Personaje
+from Planeta import Planeta
+from Vehiculo import Vehiculo
+from Mision import Mision
+
+
+
+
+
+
+
+
+
+
+
+# CREACION DE LA CLASE APP
+class App:
+    peliculas_obj=[]
+    personajes_obj=[]
+    naves_obj=[]
+    vehiculos_obj=[]
+    especies_obj=[]
+    planetas_obj=[]
+    misiones_obj=[]
+    cantidad_misiones=0
+
+
+
+
+
+
+    def cargar_API(self,link):
+        informacion=rq.get(link).json()
+        return informacion
+    
+    def start(self):
+        
+        try:
+            peliculas=self.cargar_API('https://www.swapi.tech/api/films/')
+            self.crear_peliculas
+            self.crear_personajes()
+            self.crear_especies()
+            self.crear_planetas()
+            self.crear_naves()
+            self.crear_vehiculos()
+        except:
+            print('''Está fallando la carga de la API, por conexión a internet u otro motivo.
+                  Vuelva a correr el programa.''')
--- Planeta.py
+++ Planeta.py
@@ -10,4 +10,28 @@ class Planeta:
         self.terreno=terreno
         self.superficie_acuatica=superficie_acuatica
 
+    def mostrar_planetas(self,peliculas_obj,personajes_obj):
+        print(f'\tNombre: {self.nombre}')
+        print(f'\tPeriodo de Órbita: {self.periodo_de_orbita}')
+        print(f'\tPeriodo de Rotacion: {self.periodo_de_rotacion}')
+        print(f'\tCantidad de habitantes: {self.poblacion}')
+        print(f'\tTipo de clima: {self.clima}')
+        lista_episodios=[]
+        for x in peliculas_obj:
+            for y in x.planetas:
+                if y.nombre==self.nombre:
+                    lista_episodios.append(y.nombre)
+        for episodio in lista_episodios:
+            print(f'\tEpisodio: {episodio}')
+
+        lista_personajes=[]
+        for personaje in personajes_obj:
+            if self.nombre==personaje.mundo_natal:
+                lista_personajes.append(personaje)
+        for personaje in lista_personajes:
+            episodio.mostrar_nombre_personajes()
+        print()
+
+        
+
     
