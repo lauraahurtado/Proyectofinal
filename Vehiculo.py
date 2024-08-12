b15c31abf8186b37115a8826e83b3109b1cdd383
lauraahurtado <laura.hurtado@correo.unimet.edu.ve>
2024-08-12 19:26:04 -0400

    cambioss

---
+++ Vehiculo.py
@@ -0,0 +1,14 @@
+class Vehiculo:
+    def __init__(self, nombre, modelo, clase_de_vehiculo, fabricante, costo_en_creditos, longitud, tripulacion, pasajeros, velocidad_maxima, capacidad_de_carga, consumibles, pilotos):
+        self.nombre=nombre
+        self.modelo=modelo
+        self.clase_de_vehiculo=clase_de_vehiculo
+        self.fabricante=fabricante
+        self.costo_en_creditos=costo_en_creditos
+        self.longitud=longitud
+        self.tripulacion=tripulacion
+        self.pasajeros=pasajeros
+        self.velocidad_maxima=velocidad_maxima
+        self.capacidad_de_carga=capacidad_de_carga
+        self.consumibles=consumibles
+        self.pilotos=pilotos
